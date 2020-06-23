0e81e5ad4c99e7670ae01a2d04590ea38f4e65c4
Alexander Pashchenko <alekchaik.work@gmail.com>
2020-06-23 22:06:17 +0300

    feat: add redirect to /login if not logged

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  BrowserRouter as Router, Switch, Route,
+  BrowserRouter as Router, Switch, Route, Redirect,
 } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { loggedSelector } from './auth/redux/selectors';
 import Login from './auth/components/Login';
 import SignUp from './auth/components/Signup';
 import Menu from './layout/components/Menu/Menu';
@@ -45,9 +47,10 @@ const routes = [
   },
 ];
 
-function createRoute({ title, path, component }) {
+function createRoute({ title, path, component }, isLogged) {
   return (
     <Route key={title} exact path={path}>
+      {!isLogged && <Redirect to="/login" />}
       {component || (
         <div className={styles.PageName}>
           <h1>{title}</h1>
@@ -63,12 +66,15 @@ createRoute.propTypes = {
   component: PropTypes.func.isRequired,
 };
 
-const App = () => (
-  <Router>
-    <Menu />
-    <Switch>
-      {routes.map(createRoute)}
-    </Switch>
-  </Router>
-);
+const App = () => {
+  const isLogged = useSelector(loggedSelector);
+  return (
+    <Router>
+      <Menu />
+      <Switch>
+        {routes.map((el) => createRoute(el, isLogged))}
+      </Switch>
+    </Router>
+  );
+};
 export default App;
