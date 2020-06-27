b0402085df34b3a0553569bca46a9c689e29c4ee
Denis Shiryaev <tambovchanin@gmail.com>
2020-06-27 12:35:40 +0300

    Correct usage of styed-components

--- src/layout/components/Promo/FeatureCard/FeatureCard.js
+++ src/layout/components/Promo/FeatureCard/FeatureCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FeatureWrapper from './FeatureWrapper';
-import FeatureBg from './FeatureBg';
-import FeatureTitle from './FeatureTitle';
-import FeatureText from './FeatureText';
 
 const FeatureCard = ({ src, title, text }) => (
   <FeatureWrapper>
-    <FeatureBg src={src}/>
-    <FeatureTitle>{title}</FeatureTitle>
-    <FeatureText>{text}</FeatureText>
+    <img src={src} alt={title} />
+    <h6>{title}</h6>
+    <p>{text}</p>
   </FeatureWrapper>
 );
+
+FeatureCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default FeatureCard;
--- src/layout/components/Promo/FeatureCard/FeatureWrapper.js
+++ src/layout/components/Promo/FeatureCard/FeatureWrapper.js
@@ -2,9 +2,38 @@ import styled from 'styled-components';
 
 const FeatureWrapper = styled.div`
   position: relative;
-  
+
   @media (max-width: 850px) {
     width: 100%;
+  }
+
+  img {
+    position: relative;
+    background-size: contain;
+    padding: 16px;
+    width: 100%;
+    min-height: 100px;
+  }
+
+  h6 {
+    position: absolute;
+    top: 10%;
+    right: 5%;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 130%;
+    color: #212353;
+    padding: 16px;
+    width: 54%;
+
+    @media (max-width: 570px) {
+      font-size: 22px;
+      right: 10%;
+      left: unset;
+      max-width: 60%;
+      text-align: right;
+    }
   }
 `;
 
