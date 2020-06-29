5bfe075201d648e89dfbddaf5603ca92f062a6af
Raizer58 <Raizer9058w@gmail.com>
2020-06-29 21:29:42 +0300

    fix: add local state

--- src/common/components/LevelSwitcher.js
+++ src/common/components/LevelSwitcher.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import StyleSwitcherLevel from './style.LevelSwitcher';
@@ -6,32 +6,38 @@ import StyleSwitcherLevel from './style.LevelSwitcher';
 const countLevel = 6;
 const levels = new Array(countLevel);
 
-const SwitcherLevel = ({ activeLevel, handlerOnClick }) => (
-  <StyleSwitcherLevel>
-    <div className="div">
-      <p className="p">Уровень</p>
-      <ul className="ul">
-        {
-          levels
-            .fill(' ')
-            .map((el, index) => index + 1)
-            .map((level, index) => (
-              <li
-                key={level}
-                onClick={() => handlerOnClick(index)}
-                className={activeLevel === index ? 'li active' : 'li'}
-              >
-                {level}
-              </li>
-            ))
-        }
-      </ul>
-    </div>
-  </StyleSwitcherLevel>
-);
+const SwitcherLevel = ({ handlerOnClick }) => {
+  const [activeLevel, setActiveLevel] = useState();
+
+  return (
+    <StyleSwitcherLevel>
+      <div className="div">
+        <p className="p">Уровень</p>
+        <ul className="ul">
+          {
+            levels
+              .fill(' ')
+              .map((el, index) => index + 1)
+              .map((level, index) => (
+                <li
+                  key={level}
+                  onClick={() => {
+                    setActiveLevel(level);
+                    handlerOnClick(index);
+                  }}
+                  className={activeLevel === level ? 'li active' : 'li'}
+                >
+                  {level}
+                </li>
+              ))
+          }
+        </ul>
+      </div>
+    </StyleSwitcherLevel>
+  );
+};
 
 SwitcherLevel.propTypes = {
-  activeLevel: PropTypes.number.isRequired,
   handlerOnClick: PropTypes.func.isRequired,
 };
 
