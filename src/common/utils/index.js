4ba017d224e2d7a892cd579767b9a050940842ff
Alexander Pashchenko <alekchaik.work@gmail.com>
2020-07-02 19:46:37 +0300

    feat: make a fetchJSON

--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -1,43 +1,45 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { userSelector } from '../../auth/redux/selectors';
 
-const useAPI = (url, options = {}, action, condition = true) => {
+const fetchJSON = async (endpoint, fetchOptions) => {
+  const url = `https://afternoon-falls-25894.herokuapp.com/${endpoint}`;
+  const data = await fetch(url, fetchOptions);
+  const json = await data.json();
+  if (data.status !== 200) throw new Error(data.status);
+  return json;
+};
+
+const useAPI = (endpoint, fetchOptions = {}, action, shouldFetch = true) => {
   const dispatch = useDispatch();
   const { token } = useSelector(userSelector);
   const [result, setResult] = useState();
   const [error, setError] = useState();
+  const finalOptions = useMemo(() => ({
+    ...fetchOptions,
+    'withCredentials': true,
+    'headers': {
+      ...fetchOptions.headers,
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  }), [fetchOptions, token]);
 
   useEffect(() => {
-    if (condition) {
-      const finalUrl = `https://afternoon-falls-25894.herokuapp.com/${url}`;
-      const finalOptions = {
-        ...options,
-        'withCredentials': true,
-        'headers': {
-          ...options.headers,
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      };
-      const fetchData = async () => {
-        try {
-          const data = await fetch(finalUrl, finalOptions);
-          const json = await data.json();
-          if (data.status !== 200) throw new Error(data.status);
-          if (action) action(json);
-          setResult(json);
-        } catch (er) {
-          setError(er);
-        }
-      };
-
-      fetchData();
+    if (shouldFetch) {
+      fetchJSON(endpoint, finalOptions)
+        .then((data) => {
+          setResult(data);
+          if (action) action(data);
+        })
+        .catch((er) => setError(er));
     }
-  }, [url, options, token, action, dispatch, condition]);
+  }, [endpoint, finalOptions, action, dispatch, shouldFetch]);
+
   if (error) console.log(error);
+
   return result;
 };
 
