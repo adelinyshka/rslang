181c098949a0600127b19bed2c3f747b4030f1da
Alexander Pashchenko <alekchaik.work@gmail.com>
2020-07-08 21:04:13 +0500

    feat: add adaptive styling

---
+++ src/statistics/components/GamesStat/GamesStat.js
@@ -0,0 +1,117 @@
+import React, {
+  useMemo, useState, useCallback, useEffect,
+} from 'react';
+import { useSelector } from 'react-redux';
+import { Modal, Button, Table } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+import useAPI from '../../../common/utils';
+
+import { userIdSelector } from '../../../auth/redux/selectors';
+
+import styles from './GamesStat.module.css';
+
+const fetchOptions = {
+  method: 'GET',
+};
+
+const titles = {
+  'puzzle': 'English Puzzle',
+  'speakit': 'Speakit',
+  'savannah': 'Саванна',
+  'audiocall': 'Аудиовызов',
+  'sprint': 'Спринт',
+  'memory': 'Память',
+};
+
+const GamesStat = ({ show, onHide }) => {
+  const userId = useSelector(userIdSelector);
+  const [statistics, setStatistics] = useState();
+  const [tableBody, setTableBody] = useState();
+
+  const endpoint = useMemo(() => `users/${userId}/statistics`, [userId]);
+  const action = useCallback(({ optional }) => {
+    setStatistics(optional);
+  }, [setStatistics]);
+  useAPI(endpoint, fetchOptions, action);
+
+  useEffect(() => {
+    if (statistics) {
+      const rows = [];
+      const values = Object.values(statistics);
+      const maxIndex = values
+        .reduce((acc, cur) => {
+          const { length } = Object.keys(cur);
+          return (length > acc ? length : acc);
+        }, 0);
+      for (let i = 0; i < maxIndex; i++) {
+        const date = ['дата'];
+        const res = ['результат'];
+        for (let j = 0; j < 6; j++) {
+          const keys = Object.keys(values[j]);
+          date.push(keys[i]);
+          res.push(values[j][keys[i]]);
+        }
+        const jsxDate = date.map((el, index) => <td key={el + index}>{el}</td>);
+        const jsxRes = res.map((el, index) => <td key={el + index}>{el}</td>);
+        rows.push(jsxDate);
+        rows.push(jsxRes);
+      }
+      const rowsJSX = rows.map((row, i) => <tr key={`row${i}`}>{row}</tr>);
+      setTableBody(rowsJSX);
+    }
+  }, [statistics]);
+  const tableHeader = useMemo(() => statistics
+  && Object.keys(statistics).map((key) => <th key={key}>{titles[key]}</th>),
+  [statistics]);
+
+  return (
+    <Modal
+      className={styles.Modal}
+      show={show}
+      onHide={onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className={styles.Title}>Мини-игры</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {statistics && (
+          <Table
+            className={styles.Table}
+            striped
+            bordered
+            size="sm"
+            responsive
+          >
+            <thead>
+              <tr>
+                <th>{' '}</th>
+                {tableHeader}
+              </tr>
+            </thead>
+            <tbody>
+              {tableBody}
+            </tbody>
+          </Table>
+        )}
+        {!statistics && <h1>Нет Результатов</h1>}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          className={styles.CloseBtn}
+          variant="secondary"
+          onClick={onHide}
+        >
+            Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+GamesStat.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
+};
+
+export default GamesStat;
--- src/statistics/components/Statistics/Statistics.js
+++ src/statistics/components/Statistics/Statistics.js
@@ -1,11 +1,15 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 import DoughnutChart from '../Charts/DoughnutChart';
 import BarChart from '../Charts/BarChart';
+import GamesStat from '../GamesStat/GamesStat';
+
 import styles from './Statistics.module.css';
 
 const Statistics = () => {
+  const [showGamesStat, setShowGamesStat] = useState(true);
+
   const passedCards = 54; // из api
   const rightAnswers = 50; // из api
   const newWords = 30; // из api
@@ -16,7 +20,9 @@ const Statistics = () => {
   );
   // заглушка, из api (t - не магическое число, так надо по документации графиков)
   const barData = useMemo(
-    () => Array(20).fill(null).map((el, i) => ({ t: new Date(Date.UTC(2020, 7, i)), y: i * i })), [],
+    () => Array(20).fill(null).map((el, i) => (
+      { t: new Date(Date.UTC(2020, 7, i)), y: i * i }
+    )), [],
   );
 
   const todayCardsInfo = useMemo(() => ([
@@ -56,6 +62,7 @@ const Statistics = () => {
 
   return (
     <div className={styles.StatisticsContainer}>
+      <GamesStat show={showGamesStat} onHide={() => setShowGamesStat(false)} />
       <div className={styles.Today}>
         <h2>Сегодня</h2>
         <div className={styles.TodayContainer}>
@@ -67,6 +74,7 @@ const Statistics = () => {
         <Button
           type="button"
           style={{ backgroundColor: '#6979F8' }}
+          onClick={() => setShowGamesStat(true)}
         >
           Посмотреть
         </Button>
