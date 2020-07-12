159b47b4489a7a2bcea364b1e4e3d392251f7612
Alexander Pashchenko <alekchaik.work@gmail.com>
2020-07-12 12:35:08 +0500

    fix: progress status in dictionary

--- src/dictionary/components/ProgressBar/ProgressBar.js
+++ src/dictionary/components/ProgressBar/ProgressBar.js
@@ -6,20 +6,20 @@ const ProgressBar = ({ progressStatus }) => {
   const [progressClass, setProgressClass] = useState(null);
 
   useEffect(() => {
-    switch (progressStatus) {
-      case 5:
+    switch (true) {
+      case (progressStatus >= 5):
         setProgressClass(styles.Cyan);
         break;
-      case 4:
+      case progressStatus === 4:
         setProgressClass(styles.DarkGreen);
         break;
-      case 3:
+      case progressStatus === 3:
         setProgressClass(styles.Green);
         break;
-      case 2:
+      case progressStatus === 2:
         setProgressClass(styles.LightGreen);
         break;
-      case 1:
+      case progressStatus === 1:
         setProgressClass(styles.Orange);
         break;
       default:
